2ce43b332d2afa28b5300648c161f0417a0c7860
farisubuntu <farisubuntu@gmail.com>
2024-05-24 08:31:34 +0300

    asdf

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,18 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Welcome to next JS Course 2024</h1>
+      <hr />
+      <Link href='/products' className="button">
+        Navigate to products page
+      </Link>
+      <Link href='/account' className="button">
+        Navigate to Account Page
+      </Link>
+      <h2 >Alternative way using <code>useRouter</code></h2>
     </main>
   );
 }
