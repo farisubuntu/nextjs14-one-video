bc6c3b49b2337b3f0c8ed325b20f770a06c4ab29
farisubuntu <farisubuntu@gmail.com>
2024-05-24 09:19:25 +0300

    asdf

--- src/app/account/page.jsx
+++ src/app/account/page.jsx
@@ -1,5 +1,13 @@
+import { redirect } from "next/navigation";
 
 
 export default function AccountPage() {
+  
+  // assume that profile info is 'null'
+  const userProfileInfo=null;
+
+  if(userProfileInfo === null){
+    redirect('/profile');
+  }
   return <h1>AccountPage</h1>;
 }
